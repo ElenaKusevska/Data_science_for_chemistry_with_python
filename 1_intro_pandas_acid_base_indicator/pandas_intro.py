12033e86058630ced8a6b44a4577f4c9b07e1f3f
ElenaKusevska <elena.kusevska@gmail.com>
2020-07-20 18:51:28 +0200

    pandas

--- 1_intro_pandas_acid_base_indicator/pandas_intro.py
+++ 1_intro_pandas_acid_base_indicator/pandas_intro.py
@@ -8,7 +8,14 @@ df = pandas.read_csv("Acid_base_indicators_table.csv", header=None)
 headers = ["Indicator Name", "pH start", "pH end", "pKa", "Low pH color", "High pH color"]
 df.columns = headers
 
+# Print the first five rows:
+print(df.head(5))
 
+# Print the last five rows:
+print(df.tail(5))
 
-print(df)
-print(df.head(3))
+# Print columns
+print(df.columns)
+
+# Return a Series with the data type of each column:
+print(df.dtypes)
